415b810bb35bf343bc06f424a12d37e1e4b331d6
XIANG DONG HU <huxd@cn.ibm.com>
2021-08-31 10:59:56 +0800

    test

--- test.c
+++ test.c
@@ -6,8 +6,20 @@ int main(int argc, char **argv) {
 
     SHA256Context ctx;
     int ret;
+    char msg[] = "abc";
+    uint8_t digest_buf[SHA256HashSize];
+    
     ret = SHA256Reset(&ctx);
-    printf("%d", ret);
+    printf("reset return: %d\n", ret);
+    ret = SHA256Input(&ctx, msg, sizeof(msg)-1);
+    printf("input return: %d\n", ret);
+    ret = SHA256Result(&ctx, digest_buf);
+    printf("result return: %d\n", ret);
+    puts("result:");
+    for (int i=0;i<SHA1HashSize;i++) {
+        printf("%x", (digest_buf[i] >> 4) & 0xF);
+    }
+    puts("\n");
 
     return 0;
 }
