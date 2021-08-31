79fc5674b7d68f3470c6aada41297aea541d38cd
XIANG DONG HU <huxd@cn.ibm.com>
2021-08-31 10:29:35 +0800

    test

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "sha.h"
 
 int main(int argc, char **argv) {
     puts("test.");
+
+    SHA256Context ctx;
+    int ret;
+    ret = SHA256Reset(&ctx);
+    printf("%d", ret);
+
     return 0;
 }
